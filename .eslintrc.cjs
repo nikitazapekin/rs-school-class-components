9e565bccb093532fadbb62074283e907bf8304ad
nikitazapekin <wotblitz362@mail.ru>
2024-07-10 06:31:06 +0300

    df

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,16 +1,22 @@
 module.exports = {
 	root: true,
 	env: { browser: true, es2020: true },
-	extends: [
-		'eslint:recommended',
-		'plugin:@typescript-eslint/recommended',
-		'plugin:react-hooks/recommended'
-	],
+	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended'],
 	ignorePatterns: ['dist', '.eslintrc.cjs'],
 	parser: '@typescript-eslint/parser',
-	plugins: ['react-refresh'],
+	plugins: ['react-refresh', 'react-compiler'],
 	rules: {
 		'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-		'@typescript-eslint/ban-types': 'off'
+		'react-compiler/react-compiler': 'error',
+		'@typescript-eslint/no-explicit-any': 'error',
+		'@typescript-eslint/ban-types': [
+			'error',
+			{
+				extendDefaults: true,
+				types: {
+					'{}': false,
+				},
+			},
+		],
 	},
 };
--- src/API/index.ts
+++ src/API/index.ts
@@ -1,55 +1,81 @@
 import axios, { AxiosError, AxiosResponse } from 'axios';
+
+type UserDataArray = Array<{
+	login: string;
+	id: number;
+	node_id: string;
+	avatar_url: string;
+	gravatar_id: string;
+	url: string;
+	html_url: string;
+	followers_url: string;
+	following_url: string;
+	gists_url: string;
+	starred_url: string;
+	subscriptions_url: string;
+	organizations_url: string;
+	repos_url: string;
+	events_url: string;
+	received_events_url: string;
+	type: string;
+	site_admin: boolean;
+	score: number;
+}>;
 interface UserData {
 	total_count: number;
 	incomplete_results: boolean;
-	items: [
-		{
-			login: string;
-			id: number;
-			node_id: string;
-			avatar_url: string;
-			gravatar_id: string;
-			url: string;
-			html_url: string;
-			followers_url: string;
-			following_url: string;
-			gists_url: string;
-			starred_url: string;
-			subscriptions_url: string;
-			organizations_url: string;
-			repos_url: string;
-			events_url: string;
-			received_events_url: string;
-			type: string;
-			site_admin: boolean;
-			score: number;
-		},
-	];
-} 
+	items: UserDataArray;
+}
 
-async function fetchUserData(url: string): Promise<UserData> {
+async function fetchUserData(url: string): Promise<UserDataArray> {
+	try {
+		const response: AxiosResponse<UserDataArray> = await axios.get(url);
+		return response.data;
+	} catch (error) {
+		if (axios.isAxiosError(error)) {
+			const axiosError = error as AxiosError;
+			console.error('Error fetching data:', axiosError.message);
+		} else {
+			console.error('Unknown error:', error);
+		}
+		throw new Error('Error fetching data');
+	}
+}
+async function fetchUserDataAdvanced(url: string): Promise<UserData> {
 	try {
 		const response: AxiosResponse<UserData> = await axios.get(url);
 		return response.data;
 	} catch (error) {
 		if (axios.isAxiosError(error)) {
 			const axiosError = error as AxiosError;
-			console.error('Ошибка при получении данных:', axiosError.message);
+			console.error('Error fetching data:', axiosError.message);
 		} else {
-			console.error('Неизвестная ошибка:', error);
+			console.error('Unknown error:', error);
 		}
-		throw new Error('Ошибка при получении данных');
+		throw new Error('Error fetching data');
 	}
 }
-
-export async function getUserData(limit: number, offset: number, typedValue: string): Promise<UserData> {
-	const url = `https://api.github.com/search/users?q=${typedValue}&page=${offset}&per_page=${limit}`;
-	try {
-		const data = await fetchUserData(url);
-		console.log('Данные:', data);
-		return data;
-	} catch (error) {
-		console.error(error);
-		throw error;
+export async function getUserData(limit: number, offset: number, typedValue: string): Promise<UserDataArray> {
+	let url: string;
+	if (typedValue.trim() === '') {
+		url = `https://api.github.com/users?page=${offset}&per_page=${limit}`;
+		try {
+			const data = await fetchUserData(url);
+			console.log('Data:', data);
+			return data;
+		} catch (error) {
+			console.error(error);
+			throw error;
+		}
+	} else {
+		url = `https://api.github.com/search/users?q=${typedValue}&page=${offset}&per_page=${limit}`;
+		try {
+			const data = await fetchUserDataAdvanced(url);
+			console.log('Data:', data);
+			return data.items;
+		} catch (error) {
+			console.error(error);
+			throw error;
+		}
 	}
 }
