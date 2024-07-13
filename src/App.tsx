439fa72a599d24c06169146fe2a5f5c994ee29c5
nikitazapekin <wotblitz362@mail.ru>
2024-07-13 19:22:00 +0300

    page 404

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,14 @@ import './global.scss';
 import './normalize.scss';
 import ErrorComponent from './components/ErrorComponent';
 import useSearch from './hooks/useSearch';
-
+import './App.scss';
+import { useNavigate } from 'react-router-dom';
 const App = () => {
+	const navigate = useNavigate();
 	const { handleInputChange, handleClick, isFetching, users, handleNext, handlePrev, query } = useSearch();
-
+	const handleRedirect = () => {
+		navigate('not-existing-page');
+	};
 	return (
 		<>
 			<ErrorBoundary>
@@ -26,109 +30,13 @@ const App = () => {
 						<ErrorComponent />
 					</ErrorBoundary>
 				</div>
-
-
-
-
-
-
-
-
-
-
-
-
-
- 
+				<button className="redirect__btn" onClick={handleRedirect}>
+					{' '}
+					Redirect to error page
+				</button>
 			</ErrorBoundary>
 		</>
 	);
 };
 
 export default App;
-
-//https://jestjs.io/ru/docs/tutorial-react
-//npm install --save-dev react-test-renderer
-//npm install --save-dev jest babel-jest @babel/preset-env @babel/preset-react react-test-renderer
-//npm install --save-dev @babel/preset-typescript @babel/core @babel/preset-env babel-jest
-
-
-//npm install --save-dev @testing-library/react @testing-library/dom
-//npm install --save-dev @testing-library/react @testing-library/dom @types/react @types/react-dom
-/*module.exports = {
-    presets: [
-      '@babel/preset-env',
-      ['@babel/preset-react', {runtime: 'automatic'}],
-    ],
-  };
-*/
-// babel.config.js
-
-/*
-module.exports = {
-  presets: [
-    '@babel/preset-env',
-    '@babel/preset-react',  
-    '@babel/preset-typescript' 
-  ],
-};
-
- */
-
-/*
-module.exports = {
-  presets: [
-    [
-      '@babel/preset-env',
-      {
-        targets: {
-          node: 'current'
-        }
-      }
-    ]
-  ]
-};
-*/
-//
-
-
-
-
-/*
-
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
-*/
