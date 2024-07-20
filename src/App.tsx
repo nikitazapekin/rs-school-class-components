b79aad813db9ac5f4069c44c2098f4eabf87095e
nikitazapekin <wotblitz362@mail.ru>
2024-07-20 11:12:33 +0300

    download .csv

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
- 
+ /*
 import React, { useContext, useEffect } from 'react';
 import Header from './components/Header';
 import List from './components/List';
@@ -18,49 +18,12 @@ import { storedUsersSelector } from './store/selectors/getStoredElements';
 import StoredUsersButton from './components/StoredUsersButton';
 import Modal from './components/Modal';
 
-/*
  
-import { Parser } from 'json2csv';
-import { saveAs } from 'file-saver';
-
-const ExportCSV = ( data: string ) => {
-  const exportToCSV = () => {
-    const json2csvParser = new Parser();
-    const csv = json2csvParser.parse(data);
-
-    const blob = new Blob([csv], { type: 'text/csv;charset=utf-8;' });
-    saveAs(blob, 'data.csv');
-  };
-
-  return (
-    <button onClick={exportToCSV}>
-      Экспортировать в CSV
-    </button>
-  );
-};
-
-
-
-npm install react-csv-export --save --legacy-peer-deps
-
-import CsvLink from 'react-csv-export';
-const EXAMPLE_DATA_OF_DEMO = [
-	{ id: "id-0", name: "Youngjae", age: 29, country: "KOR" },
-	{ id: "id-1", name: "Kiook", age: 20, country: "JAP" },
-	{ id: "id-2", name: "Brian", age: 30, country: "USA" },
-	{ id: "id-3", name: "Kisoo", age: 41, country: "DPR" },
-	{ id: "id-4", name: "Youngsik", age: 51, country: "GER" }
-];
-
-
-npm i react-csv --legacy-peer-deps
-npm i --save-dev @types/react-csv --legacy-peer-deps
-*/
 
 import {CSVLink, CSVDownload} from 'react-csv';
-const csvData =[
-	['rahul', 'delhi', 'accountsdept']  
-  ];
+
+ 
+ const csvData = "bshb"
 const App = () => {
 
 	const storedUsers = useSelector(storedUsersSelector)
@@ -114,28 +77,84 @@ console.log(storedUsers)
 };
 
 export default App;
+ */
+
+
+import Header from './components/Header';
+import List from './components/List';
+import { ErrorBoundary } from './components/ErrorBoundary';
+
+import React, { useEffect } from 'react';
+import { CSVLink } from 'react-csv';
+import { useSelector } from 'react-redux';
+//import { useSearch, storedUsersSelector } from './yourCustomHooks'; // Ensure to import your custom hooks correctly
+//import { useLazyGetPokemonByNameQuery } from './yourRTKQueries'; // Ensure to import your RTK query hook correctly
+//import { ErrorBoundary, ThemeProvider, Header, List, StoredUsersButton, Modal, ErrorComponent, Background } from './yourComponents'; // Ensure to import your components correctly
+import { storedUsersSelector } from './store/selectors/getStoredElements';
+import StoredUsersButton from './components/StoredUsersButton';
+import useSearch from './hooks/useSearch';
 
 
 
+import  { ThemeProvider } from './components/ThemeContext';
+import Background from './components/Background';
+ import ErrorComponent from './components/ErrorComponent';
+import Modal from './components/Modal';
+//import trigger
+const App = () => {
+    const storedUsers = useSelector(storedUsersSelector);
 
-/*
-<ExportCSV  data={"dds"} />
-// App.tsx
-import React from 'react';
-import { UserProvider } from './UserContext';
-import UserProfile from './UserProfile';
+    const { handleInputChange, handleClick, isFetching, users, handleNext, handlePrev, query } = useSearch();
+    //const [triggerGetPokemonByName, { data, error, isLoading }] = useLazyGetPokemonByNameQuery();
 
-const App: React.FC = () => {
-  return (
-	<UserProvider>
-	  <div className="App">
-		<h1>Welcome to the User Profile App</h1>
-		<UserProfile />
-	  </div>
-	</UserProvider>
-  );
-}
+    const handleButtonClick = () => {
+     //   triggerGetPokemonByName('bulbasaur');
+    };
+
+    useEffect(() => {
+        console.log(storedUsers);
+    }, [storedUsers]);
+
+    const csvHeaders = [
+        { label: 'Username', key: 'username' },
+        { label: 'Telephone', key: 'telephone' },
+        { label: 'ID', key: 'id' }
+    ];
+
+ 
+    const csvData = [
+        { username: 'user1', telephone: '123-456-7890', id: '1' },
+        { username: 'user2', telephone: '098-765-4321', id: '2' }
+    ];
+
+    return (
+        <>
+            <ErrorBoundary>
+                <ThemeProvider>
+                    <div className="container">
+                        <Header handleClick={handleClick} handleInputChange={handleInputChange} />
+                        <List
+                            handleNext={handleNext}
+                            handlePrev={handlePrev}
+                            users={users}
+                            isFetching={isFetching}
+                            typedValue={query}
+                        />
+                        {storedUsers.length > 0 && (
+                            <StoredUsersButton />
+                        )}
+                        <Modal />
+                        <ErrorBoundary>
+                            <ErrorComponent />
+                        </ErrorBoundary>
+                    </div>
+                    <button onClick={handleButtonClick}>Fetch Pokemon</button>
+                    <Background />
+                    <CSVLink data={csvData} headers={csvHeaders} filename={"users.csv"}>Download me</CSVLink>
+                </ThemeProvider>
+            </ErrorBoundary>
+        </>
+    );
+};
 
 export default App;
-
-*/
--- src/components/ModalCard/index.tsx
+++ src/components/ModalCard/index.tsx
@@ -1,8 +1,93 @@
+import React from 'react';
+import { clearStoredElementsByIdActionCreator } from "@/store/action-creators/removeStoredElementByIdActionCreator";
+import "./index.scss";
+import { useAppDispatch } from "@/hooks/redux";
+import { CSVLink } from 'react-csv';
 
+interface UserItem {
+    user: {
+        login: string;
+        id: number;
+        node_id: string;
+        avatar_url: string;
+        gravatar_id: string;
+        url: string;
+        html_url: string;
+        followers_url: string;
+        following_url: string;
+        gists_url: string;
+        starred_url: string;
+        subscriptions_url: string;
+        organizations_url: string;
+        repos_url: string;
+        events_url: string;
+        received_events_url: string;
+        type: string;
+        site_admin: boolean;
+        score: number;
+    }
+}
+
+const ModalCard = ({ user }: UserItem) => {
+    const dispatch = useAppDispatch();
+
+    const handleRemove = () => {
+        dispatch(clearStoredElementsByIdActionCreator(user.id));
+    };
+
+    const csvHeaders = [
+        { label: 'Login', key: 'login' },
+        { label: 'Id', key: 'id' },
+        { label: 'Node ID', key: 'node_id' },
+        { label: 'Avatar URL', key: 'avatar_url' },
+        { label: 'Gravatar ID', key: 'gravatar_id' },
+        { label: 'URL', key: 'url' },
+        { label: 'HTML URL', key: 'html_url' },
+        { label: 'Followers URL', key: 'followers_url' },
+        { label: 'Following URL', key: 'following_url' },
+        { label: 'Gists URL', key: 'gists_url' },
+        { label: 'Starred URL', key: 'starred_url' },
+        { label: 'Subscriptions URL', key: 'subscriptions_url' },
+        { label: 'Organizations URL', key: 'organizations_url' },
+        { label: 'Repos URL', key: 'repos_url' },
+        { label: 'Events URL', key: 'events_url' },
+        { label: 'Received Events URL', key: 'received_events_url' },
+        { label: 'Type', key: 'type' },
+        { label: 'Site Admin', key: 'site_admin' },
+        { label: 'Score', key: 'score' }
+    ];
+
+    const csvData = [user];
+
+    return (
+        <div className="modal__card">
+            <div className="modal__card__inner">
+                <img src={user.avatar_url} alt="avatar" className="modal__card__avatar" />
+                <p className="modal__card__title">
+                    {user.login}
+                </p>
+                <div className="modal__card__btns">
+                    <CSVLink data={csvData} headers={csvHeaders} filename={`${user.login}_details.csv`}>
+                        <button className="modal__card__btn">Download</button>
+                    </CSVLink>
+                    <button className="modal__card__btn modal__card__clear" onClick={handleRemove}>Delete</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ModalCard;
+
+
+
+
+
+/*
 import { clearStoredElementsByIdActionCreator } from "@/store/action-creators/removeStoredElementByIdActionCreator";
 import "./index.scss"
 import { useAppDispatch } from "@/hooks/redux";
- 
+import { CSVLink } from 'react-csv';
 interface UserItem {
     user: {
 
@@ -33,8 +118,24 @@ const ModalCard = ({ user }: UserItem) => {
     const dispatch = useAppDispatch()
     const handleRemove = () => {
         dispatch(clearStoredElementsByIdActionCreator(user.id))
-     //   if()
+     
     }
+
+
+    const csvHeaders = [
+     
+         {label: 'Login', key: 'login'},
+    {label: 'Id', key: 'id'},
+    {label: 'Node_id',  key: "node_id"},
+
+    ];
+
+ 
+    const csvData = [
+        { username: 'user1', telephone: '123-456-7890', id: '1' },
+        { username: 'user2', telephone: '098-765-4321', id: '2' }
+    ];
+
     return (
         <div className="modal__card">
             <div className="modal__card__inner">
@@ -45,36 +146,78 @@ const ModalCard = ({ user }: UserItem) => {
                 <div className="modal__card__btns">
                     <button className="modal__card__btn">Download</button>
                     <button className="modal__card__btn modal__card__clear" onClick={handleRemove}>Delete</button>
+                    <CSVLink data={csvData} headers={csvHeaders} filename={"users.csv"}>Download me</CSVLink>
                 </div>
             </div>
         </div>
     );
 }
 export default ModalCard;
+ 
+*/
 
 /*
-import "./index.scss"
-interface ModalCardProps {
-    avatar: string;
-    id: number,
-    login: string
-}
-const ModalCard = ({ avatar, id, login }: ModalCardProps) => {
-    return (
-        <div className="modal__card">
-            <div className="modal__card__inner">
-                <img src={avatar} alt="avatar" className="modal__card__avatar" />
-                <p className="modal__card__title">
-                    {login}
-                </p>
-                <div className="modal__card__btns">
-                    <button className="modal__card__btn">Download</button>
-                    <button className="modal__card__btn modal__card__clear">Delete</button>
-                </div>
-            </div>
-        </div>
-    );
-}
-export default ModalCard;
 
+import  { ThemeProvider } from './components/ThemeContext';
+import Background from './components/Background';
+ import ErrorComponent from './components/ErrorComponent';
+import Modal from './components/Modal';
+//import trigger
+const App = () => {
+    const storedUsers = useSelector(storedUsersSelector);
+
+    const { handleInputChange, handleClick, isFetching, users, handleNext, handlePrev, query } = useSearch();
+    //const [triggerGetPokemonByName, { data, error, isLoading }] = useLazyGetPokemonByNameQuery();
+
+    const handleButtonClick = () => {
+     //   triggerGetPokemonByName('bulbasaur');
+    };
+
+    useEffect(() => {
+        console.log(storedUsers);
+    }, [storedUsers]);
+
+    const csvHeaders = [
+        { label: 'Username', key: 'username' },
+        { label: 'Telephone', key: 'telephone' },
+        { label: 'ID', key: 'id' }
+    ];
+
+ 
+    const csvData = [
+        { username: 'user1', telephone: '123-456-7890', id: '1' },
+        { username: 'user2', telephone: '098-765-4321', id: '2' }
+    ];
+
+    return (
+        <>
+            <ErrorBoundary>
+                <ThemeProvider>
+                    <div className="container">
+                        <Header handleClick={handleClick} handleInputChange={handleInputChange} />
+                        <List
+                            handleNext={handleNext}
+                            handlePrev={handlePrev}
+                            users={users}
+                            isFetching={isFetching}
+                            typedValue={query}
+                        />
+                        {storedUsers.length > 0 && (
+                            <StoredUsersButton />
+                        )}
+                        <Modal />
+                        <ErrorBoundary>
+                            <ErrorComponent />
+                        </ErrorBoundary>
+                    </div>
+                    <button onClick={handleButtonClick}>Fetch Pokemon</button>
+                    <Background />
+                    <CSVLink data={csvData} headers={csvHeaders} filename={"users.csv"}>Download me</CSVLink>
+                </ThemeProvider>
+            </ErrorBoundary>
+        </>
+    );
+};
+
+export default App;
 */
