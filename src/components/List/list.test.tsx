e4055d2a4813ba61f3d1f3cc8a0e41cdcf985e3f
nikitazapekin <wotblitz362@mail.ru>
2024-07-26 13:14:55 +0300

    list test

--- src/components/List/list.test.tsx
+++ src/components/List/list.test.tsx
@@ -1,7 +1,80 @@
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
+import { MemoryRouter } from 'react-router-dom';
+import List from './index.tsx';
+import * as reduxHooks from 'react-redux';
+import { render } from '@testing-library/react';
+
+jest.mock('react-redux');
+jest.mock('react-router-dom', () => ({
+    ...jest.requireActual('react-router-dom'),
+    useNavigate: jest.fn(),
+}));
+
+const mockStore = configureStore([]);
+const initialState = {
+    params: { offset: 0, query: '' },
+    users: [],
+    isLoading: false,
+};
+const store = mockStore(initialState);
+
+describe("List Component", () => {
+    it('renders correctly', () => {
+        const mockDispatchFn = jest.fn();
+        jest.spyOn(reduxHooks, 'useDispatch').mockReturnValue(mockDispatchFn);
+        jest.spyOn(reduxHooks, 'useSelector').mockImplementation(selector => selector(initialState));
+
+        const component = render(
+            <Provider store={store}>
+                <MemoryRouter>
+                    <List />
+                </MemoryRouter>
+            </Provider>
+        );
+
+        expect(component).toMatchSnapshot();
+    });
+});
+
 
 
 
 /*
+import { MemoryRouter } from "react-router-dom";
+import List from "./index.tsx";
+import * as reduxHooks from "react-redux";
+import { render, 
+    //fireEvent, screen 
+
+} from "@testing-library/react";
+//import { AddElementToStorage } from "@/store/action-creators/addElementToStorage.ts";
+//import { useNavigate } from "react-router-dom";
+
+jest.mock('react-redux');
+jest.mock('react-router-dom', () => ({
+    ...jest.requireActual('react-router-dom'),
+    useNavigate: jest.fn(),
+}));
+const mockedDispatch = jest.spyOn(reduxHooks, 'useDispatch');
+
+describe("Card Component", () => {
+    it('renders correctly', () => {
+        const mockDispatchFn = jest.fn();
+        mockedDispatch.mockReturnValue(mockDispatchFn);
+
+        const component = render(
+            <MemoryRouter>
+               <List />
+            </MemoryRouter>
+        );
+
+        expect(component).toMatchSnapshot();
+    })
+    });
+*/
+/*
+
 import { MemoryRouter } from "react-router-dom";
 import Card from "./index.tsx";
 import * as reduxHooks from "react-redux";
@@ -149,4 +222,6 @@ describe("Card Component", () => {
     });
 });
 
+
+ 
 */
