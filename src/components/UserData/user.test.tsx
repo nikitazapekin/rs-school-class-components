9929563cc48616b969a9f952d00b3ff450a29a5c
nikitazapekin <wotblitz362@mail.ru>
2024-07-27 15:39:06 +0300

    users tests

--- src/components/UserData/user.test.tsx
+++ src/components/UserData/user.test.tsx
@@ -1,16 +1,19 @@
-import { render, screen, // fireEvent, 
-    waitFor } from '@testing-library/react';
+ 
+ 
+import { render, screen, 
+    waitFor 
+} from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { store } from '@/store/store';
 import { BrowserRouter } from 'react-router-dom';
-import UserData from './index'; // Adjust the import as needed
+import UserData from './index';  
 import ThemeContext, { Theme } from '../ThemeContext';
 import { jest } from '@jest/globals';
 
-// Mock ThemeContext
+ 
 const mockThemeContext: Theme = {
-  isDark: false, // or true, based on what you want to test
-  toggleTheme: jest.fn(), // mock the toggleTheme function if it's required
+  isDark: false, 
+  toggleTheme: jest.fn(), 
 };
 
 const renderWithProviders = (ui: React.ReactElement ) => {
@@ -26,40 +29,15 @@ const renderWithProviders = (ui: React.ReactElement ) => {
 };
 
 describe('UserData Component', () => {
-  test('renders spinner when loading', async () => {
-    renderWithProviders(<UserData />);
+    test('renders spinner when loading', async () => {
+        renderWithProviders(<UserData />);
+ 
+    expect(screen.getByTestId('loader')).toBeInTheDocument();
     
-    // Ensure spinner shows up
-    expect(screen.getByTestId('spinner')).toBeInTheDocument();
-    
-    // Wait for data to be loaded and check if spinner is removed
-    await waitFor(() => expect(screen.queryByTestId('spinner')).toBeNull());
-  });
-/*
-  test('displays user data when available', async () => {
-    renderWithProviders(<UserData />);
-    
-    // Wait for user data to be fetched and displayed
-    await waitFor(() => {
-      expect(screen.getByText('testuser')).toBeInTheDocument();
-      expect(screen.getByAltText("testuser's avatar")).toBeInTheDocument();
-      expect(screen.getByText('ID: 1')).toBeInTheDocument();
-      expect(screen.getByText('Type: User')).toBeInTheDocument();
+   
+    await waitFor(() => expect(screen.queryByTestId('loader')).toBeNull());
     });
-  });
 
-  test('handles return button click', () => {
-    const mockNavigate = jest.fn();
-    jest.spyOn(require('react-router-dom'), 'useNavigate').mockReturnValue(mockNavigate);
-    
-    renderWithProviders(<UserData />);
-    
-    // Simulate button click
-    fireEvent.click(screen.getByText('Close'));
-    
-    // Check if navigate function was called
-    expect(mockNavigate).toHaveBeenCalledWith('/main');
-  });
-*/
-  // More tests can be added here to cover other scenarios
+   
 });
+  
--- src/store/selectors/isLoadingSelector.ts
+++ src/store/selectors/isLoadingSelector.ts
@@ -1,2 +1,2 @@
 import { RootState } from "../store";
-export const isLoadingSelector = (state: RootState) => state.appSlice.isLoading
+export const isLoadingSelector = (state: RootState) => state.appSlice.isLoading  
