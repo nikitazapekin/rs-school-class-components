0d73b76f966441ab5849108030641fe631aea4b5
nikitazapekin <wotblitz362@mail.ru>
2024-08-05 12:31:18 +0300

    remix integration

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,38 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
+import { vitePlugin as remix } from "@remix-run/dev";
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), tsconfigPaths()],
+	plugins: [react(), tsconfigPaths(),
+		remix({
+			future: {
+			  v3_fetcherPersist: true,
+			  v3_relativeSplatPath: true,
+			  v3_throwAbortReason: true,
+			},
+		  })
+
+
+	],
 });
+
+
+/*
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+  plugins: [
+    remix({
+      future: {
+        v3_fetcherPersist: true,
+        v3_relativeSplatPath: true,
+        v3_throwAbortReason: true,
+      },
+    }),
+    tsconfigPaths(),
+  ],
+});
+
+*/
