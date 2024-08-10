0a8e9c7f22ca2b51433499e4471d0cd2c7317a53
nikitazapekin <wotblitz362@mail.ru>
2024-08-10 23:52:57 +0300

    theme test

---
+++ __tests__/theme.test.tsx
@@ -0,0 +1,81 @@
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import { describe, it, expect, vi } from 'vitest';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import ThemeButton from '../src/components/ThemeButton';
+import { rootReducer } from '../src/store/store';
+import { setTheme } from '../src/store/slices/themeSlice';
+
+// Мокаем хук useAppDispatch
+const mockDispatch = vi.fn();
+vi.mock('../src/hooks/redux', () => ({
+  useAppDispatch: () => mockDispatch,
+}));
+
+describe('ThemeButton Component', () => {
+  const initialState = {
+    themeSlice: { isDark: false },
+  };
+
+  const store = configureStore({
+    reducer: rootReducer,
+    preloadedState: initialState,
+  });
+
+  it('should render correctly with light theme', () => {
+    render(
+      <Provider store={store}>
+        <ThemeButton />
+      </Provider>
+    );
+
+    const buttonElement = screen.getByText(/Switch Theme Light/i);
+    expect(buttonElement).toBeInTheDocument();
+    expect(buttonElement).toHaveStyle({
+      backgroundColor: 'transparent',
+      color: '#000',
+    });
+  });
+
+  it('should render correctly with dark theme', () => {
+    // Изменяем начальное состояние на темную тему
+    const darkThemeState = {
+      themeSlice: { isDark: true },
+    };
+
+    const darkStore = configureStore({
+      reducer: rootReducer,
+      preloadedState: darkThemeState,
+    });
+
+    render(
+      <Provider store={darkStore}>
+        <ThemeButton />
+      </Provider>
+    );
+
+    const buttonElement = screen.getByText(/Switch Theme Dark/i);
+    expect(buttonElement).toBeInTheDocument();
+    expect(buttonElement).toHaveStyle({
+      backgroundColor: 'rgb(90, 90, 190)',
+      color: '#fff',
+    });
+  });
+
+  it('should dispatch setTheme action when clicked', () => {
+    render(
+      <Provider store={store}>
+        <ThemeButton />
+      </Provider>
+    );
+
+    const buttonElement = screen.queryAllByText(/Switch Theme Light/i);
+    buttonElement.forEach(item=> {
+
+        fireEvent.click(item);
+        expect(mockDispatch).toHaveBeenCalledWith(setTheme());
+    })
+
+  });
+});
--- src/components/ThemeButton/index.tsx
+++ src/components/ThemeButton/index.tsx
@@ -41,28 +41,4 @@ const styles: Record<string, CSSProperties> = {
 };
 
 export default ThemeButton;
-
-/*
-//import './index.scss';
-import { useSelector } from 'react-redux';
-import { getThemeSelector } from "../../store/selectors/getTheme"
-
-import { useAppDispatch } from '../../hooks/redux';
-import { setTheme } from '../../store/slices/themeSlice';
-const ThemeButton = () => {
-	const isDark  = useSelector(getThemeSelector)
-	const dispatch = useAppDispatch()
-	const handleClick = () => {
-		dispatch(setTheme())
-	}
-	return (
-		<>
-			<button className="theme__button" onClick={handleClick}>
-				Switch Theme {isDark ? 'Dark' : 'Light'}
-			</button>
-		</>
-	);
-};
-
-export default ThemeButton;
-*/
+ 
