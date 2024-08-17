63c07e524b4b2d4ed41739d79464115ab3ee7fef
nikitazapekin <wotblitz362@mail.ru>
2024-08-17 09:33:56 +0300

    refactor of uncontrolled component

--- src/components/UncontrolledForm/index.tsx
+++ src/components/UncontrolledForm/index.tsx
@@ -5,7 +5,7 @@ import { coutrySelector } from "../../store/selectors/country.selector";
 import { useAppDispatch } from "../../hooks/redux";
 import { SetPersonalDataReactHookFormActionCreator } from "../../store/actions/setUserData";
 import { useNavigate } from "react-router-dom";
- 
+ import {schema} from "./schema"
 import * as yup from "yup";
 
 const UncontrolledForm = () => {
@@ -25,74 +25,7 @@ const UncontrolledForm = () => {
     const fileInputRef = useRef<HTMLInputElement>(null);
 
     const errorRef = useRef<{ [key: string]: HTMLParagraphElement | null }>({});
-
-    const schema = yup.object().shape({
-        name: yup
-            .string()
-            .trim()
-            .required("Field is required")
-            .min(2, "The name must be at least 2 characters long")
-            .test("is-first-letter-uppercase", "The first character must be uppercase", (value) => {
-                if (!value) return false;
-                return /^[A-ZА-Я]/.test(value);
-            }),
-
-        email: yup
-            .string()
-            .required("Field is required")
-            .matches(/^\S+@\S+\.\S+$/, "Invalid email format"),
-
-        password: yup
-            .string()
-            .required("Field is required")
-            .min(6, "Password must be at least 6 characters long")
-            .test("password-strength", "Password is too weak", (value) => {
-                if (!value) return false;
-                const hasUpperCase = /[A-Z]/.test(value);
-                const hasLowerCase = /[a-z]/.test(value);
-                const hasNumber = /\d/.test(value);
-                const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(value);
-                const strengthCount = [
-                    hasUpperCase,
-                    hasLowerCase,
-                    hasNumber,
-                    hasSpecialChar,
-                ].filter(Boolean).length;
-                return strengthCount >= 3;
-            }),
-
-        confirmPassword: yup
-            .string()
-            .oneOf([yup.ref("password"), ""], "Passwords must match")
-            .required("Field is required"),
-
-        age: yup
-            .number()
-            .required("Field is required")
-            .positive("Age must be a positive number")
-            .integer("Age must be an integer")
-            .max(100, "Please enter a correct age"),
-
-        gender: yup
-            .string()
-            .required("Field is required")
-            .oneOf(["man", "female"], "Choose the correct gender"),
-
-        country: yup
-            .string()
-            .required("Field is required")
-            .test("is-valid-country", "Invalid country selected", function (value) {
-                return countries.includes(value || "");
-            }),
-
-        agreeToTerms: yup
-            .boolean()
-            .required("Consent required")
-            .oneOf([true], "Consent required"),
-
-        avatar: yup.string().required("Avatar is required"),
-    });
-
+ 
     const validateForm = async () => {
         const formData = {
             name: nameRef.current?.value || "",
